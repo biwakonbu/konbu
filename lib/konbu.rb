dde6b9a661c860d26a7a39f94515db9fb20fb477
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2013-12-01 17:18:11 +0900

    fix library path

---
+++ lib/konbu.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH << (File.dirname(__FILE__))
+
+require 'konbu/op'
+require 'konbu/basic_operation'
+require 'konbu/parser'
+require 'konbu/add'
+require 'konbu/sub'
+require 'konbu/mul'
+require 'konbu/div'
+
+module Konbu
+
+end
