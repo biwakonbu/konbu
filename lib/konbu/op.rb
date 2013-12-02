700c8da5a9b1687a388e9b7614e6196172999dad
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2013-12-02 21:25:34 +0900

    update basic operation spec

--- lib/konbu/op.rb
+++ lib/konbu/op.rb
@@ -1,3 +1,4 @@
+
 $LOAD_PATH << File.expand_path('../', __FILE__)
 
 require 'op/basic_operation'
--- spec/spec_op_basic_operation.rb
+++ spec/spec_op_basic_operation.rb
@@ -1,17 +1,53 @@
 require File.expand_path('../../lib/konbu/op/basic_operation', __FILE__)
+require File.expand_path('../../lib/konbu/op/add', __FILE__)
+require File.expand_path('../../lib/konbu/op/sub', __FILE__)
+require File.expand_path('../../lib/konbu/op/mul', __FILE__)
+require File.expand_path('../../lib/konbu/op/div', __FILE__)
 
 describe Konbu::BasicOperation do
   describe '#push' do
-    context 'when object is String' do
-      let(:bop) { Konbu::BasicOperation.new }
+    let(:bop) { Konbu::BasicOperation.new }
+    context 'when object is String class' do
       it 'to (fix|big)num' do
         bop.push('0000')
-        expect(bop.stack[0].class).to eq(Fixnum)
+        expect(bop.stack[-1].class).to eq(Fixnum)
       end
 
       it 'to float' do
         bop.push('1.1')
-        expect(bop.stack[0].class).to eq(Float)
+        expect(bop.stack[-1].class).to eq(Float)
+      end
+    end
+
+    context 'when object is Add class' do
+      it 'stack[-1] is Konbu::Add class object' do
+        add = Konbu::Add.new
+        bop.push(add)
+        expect(bop.stack[-1].class).to eq(Konbu::Add)
+      end
+    end
+
+    context 'when object is Sub class' do
+      it 'stack[-1] is Konbu::Sub class object' do
+        sub = Konbu::Sub.new
+        bop.push(sub)
+        expect(bop.stack[-1].class).to eq(Konbu::Sub)
+      end
+    end
+
+    context 'when object is Mul class' do
+      it 'stack[-1] is Konbu::Mul class object' do
+        mul = Konbu::Mul.new
+        bop.push(mul)
+        expect(bop.stack[-1].class).to eq(Konbu::Mul)
+      end
+    end
+
+    context 'when object is Div class' do
+      it 'stack[-1] is Konbu::Div class object' do
+        div = Konbu::Div.new
+        bop.push(div)
+        expect(bop.stack[-1].class).to eq(Konbu::Div)
       end
     end
   end
